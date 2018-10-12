3a1a0a3ee56f3ee31899db37ac009d10402de3fc
rparamo <rparamo11@gmail.com>
2018-10-11 23:54:48 -0500

    added more pics, issue with css not showing ppictures

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,128 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import FriendCard from "./components/FriendCard";
 import friends from "./friends.json";
+// import Nav from "./components/Nav";
+// import Container from "./Container";
+// import Row from "./Row";
+// import Column from "./Column";
 
-// state = {
-//   id = "",
-//   image = "",
-// };
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
-const App = () => (
-  <Wrapper>
-    <Title>Friends List</Title>
-    <FriendCard
-      // name={friends[0].name}
-      image={friends[0].image}
-      // occupation={friends[0].occupation}
-      // location={friends[0].location}
-    />
-    <FriendCard
-      // name={friends[1].name}
-      image={friends[1].image}
-      // occupation={friends[1].occupation}
-      // location={friends[1].location}
-    />
-    <FriendCard
-      // name={friends[2].name}
-      image={friends[2].image}
-      // occupation={friends[2].occupation}
-      // location={friends[2].location}
-    />
-  </Wrapper>
-);
+class App extends Component {
+  // Set this.state
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Sorry!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        {/* <Nav
+          title="Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        /> */}
+
+        <Title>
+      Click on an image to earn points and win, but dont click on an image more than once or lose!!!
+       </Title>
+
+        {/* <Container>
+          <Row>
+            {this.state.friends.map(friend => (
+              <Column size="md-3 sm-6">
+                <FriendCard
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container> */}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
+
+
+
+
+
+
+// class App extends Component {
+//   // Set this.state
+//   state = {
+//     friends,
+//     currentScore: 0,
+//     topScore: 0,
+//     rightWrong: "",
+//     clicked: [],
+//   };
+
+//   function shuffleFriends(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       let j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+//     return array;
+//   };
