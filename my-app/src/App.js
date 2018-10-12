9bf65437770a835b08c805873d1aeae70ceeb3db
rparamo <rparamo11@gmail.com>
2018-10-11 23:13:22 -0500

    added click event to friend card

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,26 +5,31 @@ import Title from "./components/Title";
 import FriendCard from "./components/FriendCard";
 import friends from "./friends.json";
 
+// state = {
+//   id = "",
+//   image = "",
+// };
+
 const App = () => (
   <Wrapper>
     <Title>Friends List</Title>
     <FriendCard
-      name={friends[0].name}
+      // name={friends[0].name}
       image={friends[0].image}
-      occupation={friends[0].occupation}
-      location={friends[0].location}
+      // occupation={friends[0].occupation}
+      // location={friends[0].location}
     />
     <FriendCard
-      name={friends[1].name}
+      // name={friends[1].name}
       image={friends[1].image}
-      occupation={friends[1].occupation}
-      location={friends[1].location}
+      // occupation={friends[1].occupation}
+      // location={friends[1].location}
     />
     <FriendCard
-      name={friends[2].name}
+      // name={friends[2].name}
       image={friends[2].image}
-      occupation={friends[2].occupation}
-      location={friends[2].location}
+      // occupation={friends[2].occupation}
+      // location={friends[2].location}
     />
   </Wrapper>
 );
--- my-app/src/components/FriendCard/FriendCard.js
+++ my-app/src/components/FriendCard/FriendCard.js
@@ -2,24 +2,14 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
+    <div className="card"
+        value={props.id}
+        onClick={() => props.handleClick(props.id)}>
+        <div className="img-container">
+            <img alt={props.name} src={props.image}
+            />
+        </div>
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-  </div>
 );
 
 export default FriendCard;
