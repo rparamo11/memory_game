f17b5a5981adcc83bc02ab07d3e331878776b9d5
rparamo <rparamo11@gmail.com>
2018-10-13 14:30:25 -0500

    Updates

---
+++ precache-manifest.5b69355813d2f4790600e1a30928bde3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25ef5a59cc76f30d5876",
+    "url": "/memory_game/static/js/runtime~main.25ef5a59.js"
+  },
+  {
+    "revision": "a380f2a5e2a5cd4fcb27",
+    "url": "/memory_game/static/js/main.a380f2a5.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/memory_game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "a380f2a5e2a5cd4fcb27",
+    "url": "/memory_game/static/css/main.068a9d9d.chunk.css"
+  },
+  {
+    "revision": "d071a63273c5b21943c8f40217cb1cae",
+    "url": "/memory_game/index.html"
+  }
+];
